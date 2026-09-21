a5e2a4f3a8d7bfb06edc7816a10bc53e2e8cc2df
ltdrobot <ltdrobot@localizethedocs.org>
2026-09-21 16:35:28 +0000

    Deploying to pages from @ localizethedocs/sphinxcontrib-rust-docs-l10n@8a4beb07229d7893833409c1a4a6c4af3d437507 🚀

---
+++ ltd-config.js
@@ -0,0 +1,22 @@
+"use strict";
+
+var CONFIG_OPTIONS = {
+  CONFIG_LANGUAGES: [
+    ["en-us",   "English"],
+    ["zh-cn",   "简体中文"],
+    ["zh-tw",   "繁體中文"],
+  ],
+  CONFIG_VERSIONS: [
+    ["latest",  "Development"],
+    ["stable",  "Stable Release"],
+    ["1",       "Release 1.x"],
+    ["0",       "Release 0.x"],
+  ],
+  CONFIG_PROJECTS: [
+    ["Index",   "https://projects.localizethedocs.org"],
+    ["Crowdin", "https://localizethedocs.crowdin.com/sphinxcontrib-rust-docs-l10n"],
+    ["GitHub",  "https://github.com/localizethedocs/sphinxcontrib-rust-docs-l10n"],
+    ["AtomGit", "https://atomgit.com/localizethedocs/sphinxcontrib-rust-docs-l10n"],
+    ["GitFlic", "https://gitflic.ru/project/localizethedocs/sphinxcontrib-rust-docs-l10n"],
+  ]
+};
